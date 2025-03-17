1ea0d49a86573e79bbdfb96171d3ea60fae90cc1
Ernest Kamau <1.kamauernest@gmail.com>
2025-03-17 15:42:28 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft(){
+    const left = dodger.style.left.replace("px", "");
+    const leftnum = parseInt(left, 10);
+
+    if (leftnum > 0){
+        dodger.style.left = `${leftnum - 1}px`
+    }
+}
+
+
+function moveDodgerRight(){
+    const right = dodger.style.left.replace("px", "");
+    const rightNum = parseInt(right, 10)
+
+    if(rightNum <= 360){
+        dodger.style.left = `${rightNum + 1}px`
+    }
+}
+
+
+
+
+document.addEventListener("keydown", (events) =>{ 
+    if (events.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+
+     if (events.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
+
+
